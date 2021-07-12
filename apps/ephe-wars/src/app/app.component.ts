aa502f9408985b62925f2073339ca484bff8577f
Tom Conley <tommyc38@gmail.com>
2021-07-12 16:31:19 -0600

    initial commit

---
+++ apps/ephe-wars/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ephesoft-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ephe-wars';
+}
