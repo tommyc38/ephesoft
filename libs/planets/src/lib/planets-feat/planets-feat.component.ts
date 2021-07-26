b48154448a174874a7a3030c73fa240f5b94f77a
Tom Conley <tommyc38@gmail.com>
2021-07-26 13:36:53 -0600

    show loading indicator when paging through planets

--- libs/planets/src/lib/planets-feat/planets-feat.component.ts
+++ libs/planets/src/lib/planets-feat/planets-feat.component.ts
@@ -10,15 +10,15 @@ import { PeopleService } from '@ephesoft/people-data';
 @Component({
   selector: 'planet-feat',
   templateUrl: './planets-feat.component.html',
-  styleUrls: ['./planets-feat.component.scss']
+  styleUrls: ['./planets-feat.component.scss'],
 })
 export class PlanetsFeatComponent implements OnInit, OnDestroy {
   planetDetail: Planet | undefined = undefined;
-  planetResidentDetail: Person[] | undefined = undefined
-  planetResponse: Response<Planet> | undefined;
-  hasError: HttpErrorResponse | undefined = undefined
-  page = 1
-  private _residents:{[planetUrl:string]:Person[]} = {}
+  planetResidentDetail: Person[] | undefined = undefined;
+  planetResponse: Response<Planet> | undefined  = undefined;
+  hasError: HttpErrorResponse | undefined = undefined;
+  page = 1;
+  private _residents: { [planetUrl: string]: Person[] } = {};
   private _paramSubscription: Subscription = Subscription.EMPTY;
 
   constructor(
@@ -34,42 +34,43 @@ export class PlanetsFeatComponent implements OnInit, OnDestroy {
     this._paramSubscription = this.route.paramMap.pipe(
       switchMap(params => {
         this.page = +(params.get('page') ?? 1);
+        this.planetResponse = undefined
         return this.planetService.getPlanets(this.page).pipe(
           catchError(err => {
-            this.hasError = err
-            return of(undefined)
+            this.hasError = err;
+            return of(undefined);
           })
         );
       }),
-      tap(response => this.planetResponse = response)
+      tap(response => { this.planetResponse = response; })
     ).subscribe();
   }
 
-  navigateToPage(url: string | null){
-    if (!this.planetResponse) return;
+  navigateToPage(url: string | null) {
+    // if (!this.planetResponse) return;
     const page = this.planetService.getPageFromUrl(url);
-    if (page){
-      this.router.navigate([`/planets/${page}`])
+    if (page) {
+      this.router.navigate([`/planets/${page}`]);
     }
   }
 
-  trackByFn(index:number, planet:Planet){
-    return planet.url
+  trackByFn(index: number, planet: Planet) {
+    return planet.url;
   }
 
   ngOnDestroy(): void {
     this._paramSubscription.unsubscribe();
   }
 
-  setPlanet(planet: Planet): void{
-    if(this._residents[planet.url]){
-      this.planetResidentDetail = this._residents[planet.url]
+  setPlanet(planet: Planet): void {
+    if (this._residents[planet.url]) {
+      this.planetResidentDetail = this._residents[planet.url];
     }
-    this.peopleService.getManyPeople(planet.residents).subscribe( people => {
-      this._residents[planet.url] = people
-      this.planetResidentDetail = people
-      this.planetDetail = planet
-    })
+    this.peopleService.getManyPeople(planet.residents).subscribe(people => {
+      this._residents[planet.url] = people;
+      this.planetResidentDetail = people;
+      this.planetDetail = planet;
+    });
 
 
   }
